beb69af1b67a13aa8636aea5db05adf1214d0705
shinya-jp <shinya0733@gmail.com>
2020-05-08 23:26:37 +0900

    [update]応用課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,13 @@
 class BookCommentsController < ApplicationController
+
 	def create
-		book =Book.find(params[:book_id])
-		comment = current_user.book_comments.new(book_comment_params)
-		comment.book_id =book.id
-			if comment.save
-				flash[:notice] = "Comment was successfully created."
-			redirect_back(fallback_location: root_path)
+		@book =Book.find(params[:book_id])
+		@book_comment = @book.book_comments.build(book_comment_params)
+		@book_comment.user_id = current_user.id
+		if @book_comment.save
+			render :index
 		else
-			redirect_back(fallback_location: root_path)
+			render :index
 		end
 	end
 
@@ -15,10 +15,10 @@ class BookCommentsController < ApplicationController
 		book = Book.find(params[:book_id])
 		@book_comment = book.book_comments.find(params[:id])
 		@book_comment.destroy
-		redirect_back(fallback_location: root_path)
+		render :index
 	end
 	private
 	def book_comment_params
-		params.require(:book_comment).permit(:comment)
+		params.require(:book_comment).permit(:comment, :book_id, :user_id)
 	end
 end
